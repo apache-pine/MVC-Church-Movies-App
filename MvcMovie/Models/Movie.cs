1a50c309e63823d37886b54b2d24dfc12a6edb00
Kylar Sorensen <kylar.sorensen@gmail.com>
2023-06-15 15:58:38 -0700

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+
+    public int MovieId { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; } = 0.00M;
+
+    [Required]
+    public string? Genre { get; set; }
+
+    [Required]
+    public string? Rating { get; set; }
+
+    [Display(Name = "Image File Name")]
+    public string? ImageFileName { get; set; }
+
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The Saratov Approach",
+                    ReleaseDate = DateTime.Parse("2013-10-9"),
+                    Genre = "Drama",
+                    Price = 19.99M,
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    Title = "17 Miracles",
+                    ReleaseDate = DateTime.Parse("2011-6-3"),
+                    Genre = "Adventure-Drama",
+                    Price = 9.99M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "The Best Two Years",
+                    ReleaseDate = DateTime.Parse("2004-9-7"),
+                    Genre = "Comedy-Drama",
+                    Price = 19.99M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "The Other Side of Heaven",
+                    ReleaseDate = DateTime.Parse("2001-12-14"),
+                    Genre = "Adventure-Drama",
+                    Price = 15.99M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "Ephraim's Rescue",
+                    ReleaseDate = DateTime.Parse("2013-5-31"),
+                    Genre = "Adventure-Drama",
+                    Price = 19.99M,
+                    Rating = "PG"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
